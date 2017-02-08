7dc0a5b6d51fd12dcff0b4a769eaee01d3907518
mshede <mshede@163.com>
2017-02-08 11:53:51 +0800

    Update EncryptUtil.java

--- java/EncryptUtil.java
+++ java/EncryptUtil.java
@@ -8,6 +8,8 @@ import java.util.TreeMap;
 import java.util.Map.Entry;
 
 public class EncryptUtil {
+	private final static String[] hexDigits = {"0", "1", "2", "3", "4", "5", "6", "7",
+            "8", "9", "a", "b", "c", "d", "e", "f"};
 	/**
 	 * generate the sign from the parameters
 	 * @param params   the parameters of the request.
@@ -22,28 +24,55 @@ public class EncryptUtil {
 	    for (Entry<String, String> param : entrys) {
 	        basestring.append(param.getKey()).append("=").append(param.getValue());
 	    }
-		String newSignature = md5(basestring.toString()).toUpperCase();
+		String newSignature = MD5Encode(basestring.toString()).toUpperCase();
 		return newSignature;
 	}
 	
-	/**
-	 * md5 encrypt
-	 * 
-	 * @author Alex Meng
-	 * @param str
-	 * @param method
-	 * @return
-	 */
-	public static String md5(String str) {
-		MessageDigest md = null;
-		String dstr = null;
-		try {
-			md = MessageDigest.getInstance("MD5");
-			md.update(str.getBytes());
-			dstr = new BigInteger(1, md.digest()).toString(16);
-		} catch (NoSuchAlgorithmException e) {
 
-		}
-		return dstr;
-	}
+
+    /**
+     * 转换字节数组为16进制字串
+     * @param b 字节数组
+     * @return 16进制字串
+     */
+    public static String byteArrayToHexString(byte[] b) {
+        StringBuilder resultSb = new StringBuilder();
+        for (byte aB : b) {
+            resultSb.append(byteToHexString(aB));
+        }
+        return resultSb.toString();
+    }
+
+    /**
+     * 转换byte到16进制
+     * @param b 要转换的byte
+     * @return 16进制格式
+     */
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0) {
+            n = 256 + n;
+        }
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    /**
+     * MD5编码
+     * @param origin 原始字符串
+     * @return 经过MD5加密之后的结果
+     */
+    public static String MD5Encode(String origin) {
+        String resultString = null;
+        try {
+            resultString = origin;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            resultString = byteArrayToHexString(md.digest(resultString.getBytes("utf-8")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultString;
+    }
 }
+
