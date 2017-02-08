791e0773cd7e6bf9f4a309d35c03655fd2c9c835
mshede <mshede@163.com>
2017-02-08 12:05:48 +0800

    Update CancelWaybill.java

--- java/CancelWaybill.java
+++ java/CancelWaybill.java
@@ -17,7 +17,7 @@ public class CancelWaybill {
 		params.put("ts", requestTime);
 		String signString = EncryptUtil.generateSign(params, secretKey);
 		params.put("sign", signString);
-		JSONObject jsonObject = HttpRequestUtil.request("POST", "/parcel/cancel", params);
+		JSONObject jsonObject = HttpRequestUtil.request("POST", "http://api.test.fo-del.com/parcel/cancel", params);
 		System.out.println(jsonObject);
 	}
 }
